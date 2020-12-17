c9d0fc52e249c09d4df5029ef25a7182b512c1a9
jooney <jooneybadger@gmail.com>
2020-12-17 23:24:48 +0900

    [modify] config.js 오타 수정

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const SIGNUP_API = `http://10.177.199.95:8000/user/signup`;
-export const SIGNIN = `http://10.177.199.95:8000/user/signin`;
+export const SIGN_IN = `http://10.177.199.95:8000/user/signin`;
