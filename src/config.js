ae9ce24ffe7c070fd9d1f06f1008ec0d54c76bc9
jooney <jooneybadger@gmail.com>
2020-12-19 19:18:42 +0900

    [add] AWS API주소 추가

--- src/config.js
+++ src/config.js
@@ -1,2 +1,4 @@
 export const SIGNUP_API = `http://10.177.199.95:8000/user/signup`;
 export const SIGNIN_API = `http://10.177.199.95:8000/user/signin`;
+
+export const AWS_API = `http://13.125.154.100:8000/user`;
