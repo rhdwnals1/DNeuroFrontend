d09127b3d29e47fdc6857ee68c43d48090ba3c86
jooney <jooneybadger@gmail.com>
2020-12-28 00:26:25 +0900

    wip

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -1,17 +1,12 @@
 import React, { useState, Fragment } from "react";
 import { Link, useHistory } from "react-router-dom";
 import { SIGNIN_API } from "../../config";
-<<<<<<< HEAD
-=======
 import { boxShadow, theme, imgUrl } from "../../styles/CommonStyle";
->>>>>>> d2646c884be6f5518cf95af11050b07408f9609b
 import styled from "styled-components";
-
 const SignIn = () => {
   const history = useHistory();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-
   const goToMain = (e) => {
     e.preventDefault();
     fetch(`${SIGNIN_API}`, {
@@ -33,24 +28,17 @@ const SignIn = () => {
         }
       });
   };
-
   const handleEmail = (e) => {
     setEmail(e.target.value);
   };
-
   const handlePassword = (e) => {
     setPassword(e.target.value);
   };
-
   return (
     <Fragment>
       <WrapSignIn>
         <Logo>
-<<<<<<< HEAD
-          <img src="/images/JM/dneuro.png" alt="logo" />
-=======
           <img src={imgUrl.logo} alt="logo" />
->>>>>>> d2646c884be6f5518cf95af11050b07408f9609b
         </Logo>
         <Login>
           <input
@@ -78,7 +66,6 @@ const SignIn = () => {
     </Fragment>
   );
 };
-
 const WrapSignIn = styled.section`
   display: flex;
   flex-direction: column;
@@ -89,14 +76,12 @@ const WrapSignIn = styled.section`
   margin: 170px auto;
   padding: 40px 30px;
 `;
-
 const Logo = styled.div`
   img {
-    width: 200px;
-    height: 60px;
+    width: 110px;
+    height: 120px;
   }
 `;
-
 const Login = styled.div`
   input {
     display: block;
@@ -110,28 +95,23 @@ const Login = styled.div`
     box-shadow: 1px 1px rgba(50, 50, 93, 0.5), 0 1px 3px rgba(0, 0, 0, 0.08);
     font-size: 15px;
     transition: 0.2s border-color, 0.2s box-shadow, 0.2s background-color;
-
     &:hover {
       cursor: text;
     }
-
     &:focus {
       background-color: #fafafa;
     }
   }
-
   .id {
     margin: 50px 0 0 0;
     border-radius: 4px 4px 0 0;
   }
-
   .pw {
     margin: 0;
     border-radius: 0 0 4px 4px;
     border-top: 0px;
   }
 `;
-
 const Button = styled.div`
   display: block;
   text-align: center;
@@ -140,24 +120,22 @@ const Button = styled.div`
   height: 50px;
   margin: 30px 0;
   padding: 13px 15px;
-  background-color: #becc01;
-  border: 1px solid #becc01;
-  border-radius: 4px;
-  color: #ffffff;
+  background-color: ${theme.pink};
+  border: 1px solid ${theme.pink};
+  border-radius: 20px;
+  color: #fff;
   font-size: 17px;
   line-height: 1.41;
   font-weight: bold;
-
   .content {
     padding-top: 2px;
   }
-
   &:hover {
     cursor: pointer;
-    background-color: #99a400;
+    background-color: ${theme.pink};
+    ${boxShadow}
   }
 `;
-
 const SignUp = styled.div`
   margin: 1px 0;
   text-align: center;
@@ -165,6 +143,14 @@ const SignUp = styled.div`
   font-size: 14px;
   font-weight: 400;
   line-height: 1.4;
+  &:hover {
+    cursor: pointer;
+    width: 80px;
+    height: 20px;
+    border: 1px solid ${theme.pink};
+    border-radius: 15px;
+    color: #ffffff;
+    background-color: ${theme.pink};
+  }
 `;
-
 export default SignIn;
